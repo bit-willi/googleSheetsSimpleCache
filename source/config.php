9a99b3bd53111e8c2053fc12246ba21c4023fcc7
Bit <willian.souza.ti@gmail.com>
2020-05-18 22:51:39 -0300

    defined environment variables

---
+++ source/config.php
@@ -0,0 +1,7 @@
+<?php
+
+define("credentialsPath", __DIR__ . "/credentials.json");
+define("tokenPath", __DIR__ . "/token.json");
+define("cachePath", __DIR__ . "/../cache/cache.csv");
+define("sheetId", "1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms");
+define("sheetRange", "A:E");
